5565f029f16b3512bb545698152f97acd537d9c2
maxfri <maxfri1995@gmail.com>
2021-09-02 02:39:16 +0300

    init: init connect to lobby component, add stories and test

---
+++ src/components/ConnectToLobby/ConnectToLobby.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { ConnectToLobby } from './ConnectToLobby';
+
+export default {
+  title: 'ConnectToLobby',
+  component: ConnectToLobby,
+} as ComponentMeta<typeof ConnectToLobby>;
+
+const Template: ComponentStory<typeof ConnectToLobby> = (args) => <ConnectToLobby {...args} />;
+
+export const Empty = Template.bind({});
+Empty.args = {
+  label: 'Connect To Lobby',
+};
---
+++ src/components/ConnectToLobby/ConnectToLobby.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import ConnectToLobby from './ConnectToLobby'
+
+describe('', () => {
+  test('should render modal window Connect to lobby', () => {
+    const { getByText } = render(<ConnectToLobby label="Connect To Lobby"/>)
+    expect(getByText(/Connect To Lobby/i)).toBeInTheDocument;
+  })
+})
+
