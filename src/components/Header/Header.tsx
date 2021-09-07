42cc87bc77bf3d743b740e464145d22a062629a0
maxfri <maxfri1995@gmail.com>
2021-09-07 10:56:09 +0300

    feat: implement default header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,22 @@
+import { Layout } from 'antd';
+import React from 'react';
+
+import logo from './logo.png';
+import './Header.css';
+
+
+export const Header: React.FC = () => {
+  return (
+    <Layout className="layout">
+      <div className="header">
+        <div className="header-logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="header-second-line">
+        </div>
+      </div>
+    </Layout>
+  )
+};
+
+export default Header;
