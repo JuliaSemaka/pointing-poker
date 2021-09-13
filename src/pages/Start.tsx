957b3e144ab5116131968b1d2f1ae3444944030f
Julia <ysemaka@mail.ru>
2021-09-13 10:00:51 +0300

    feat: correct Start

--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -1,6 +1,4 @@
-import React, { useState } from 'react';
-import { Button } from '../components/UI/Button/Button';
-import { EButtonStyle } from '../components/UI/ui.module';
+import React from 'react';
 
 export const Start: React.FC = () => {
   return <div>Start</div>;
