70e79aeb792952dc82cf4b1fa2b5f0136a993a2c
Yatsko Ivan <ivanyatsko@mail.ru>
2021-09-14 00:30:29 +0300

    feat: correct add style

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
+import 'normalize.css';
 import './App.scss';
 import { Routes } from './routes';
-import 'normalize.css';
 
 const App: React.FC = () => (
   <BrowserRouter>
