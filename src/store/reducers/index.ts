b8db7580d869e545e807a0f4148f0de2f663ee7a
Yatsko Ivan <ivanyatsko@mail.ru>
2021-09-14 00:36:57 +0300

    feat: add redux-form(main reducer for form)

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
+
 import { mainReducer } from './mainReducer';
 
 export const rootReducer = combineReducers({
   main: mainReducer,
+  form: formReducer,
 });
