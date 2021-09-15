744ba5e33edab73a9ab69350856879c56689d5fe
maxfri <maxfri1995@gmail.com>
2021-09-16 02:31:22 +0300

    feat: add main page

---
+++ src/pages/MainPage/MainPage.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import MainPage from './MainPage'
+
+describe('Main page', () => {
+  test('should render main page', () => {
+    const { getByText } = render(<MainPage/>)
+    expect(getByText(/Start your planning:/i)).toBeInTheDocument;
+  })
+})
