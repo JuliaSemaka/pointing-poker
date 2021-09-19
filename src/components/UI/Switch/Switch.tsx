d9e2ec807d3d8eb0d932b6e524786d52f9bd4480
maxfri <maxfri1995@gmail.com>
2021-09-19 16:58:27 +0300

    fix: fix types

--- src/components/UI/Switch/Switch.tsx
+++ src/components/UI/Switch/Switch.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 import './Switch.scss';
 
 interface SwitchProps {
-  isChecked: boolean;
-  label?: string;
-  handleClick: React.Dispatch<React.SetStateAction<boolean>>;
+  isChecked: boolean,
+  label?: string,
+  handleClick: React.Dispatch<React.SetStateAction<boolean>>
 }
 
 export const Switch = ({ ...props }: SwitchProps): JSX.Element => {
   const { isChecked, label, handleClick } = props;
 
-  return (
+  return ( 
     <div className="switch-wrapper">
       {label && <div className="switch-label">{label}</div>}
       <label className="switch">
