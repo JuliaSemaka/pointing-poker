60e4bace5caf1d1055772aa1442cc33de9b7f610
Julia <ysemaka@mail.ru>
2021-09-21 23:36:15 +0300

    feat: correct

--- src/app.js
+++ src/app.js
@@ -7,20 +7,11 @@ const PORT = process.env.PORT || 5000;
 const { addUser, connectionHandler, delUser } = require('./game/index');
 const { addMessage } = require('./chat/index');
 
-app.use(cors());
-app.use(express.json({ limit: '15mb' }));
+const staticFiles = path.resolve(__dirname, '../static');
 
-// {
-//   id: number,
-//   idDiller: number,
-//   isStartGame: true/false,
-//   method: "connection"/"add-user"/"del-user"
-//   users:=[{id,firstName,lastName,job,image,player(игрок,наблюд.,дилер)},..],
-//   settings: {asPlayer,changingCard,isTimer,scoreType,scoreTypeShort,roundTime:}
-//   time: number
-//   cards: [{currency,value},{}..],
-//   tasks: [{id,value,mark},..]
-// }
+app.use(json({limit: '50mb'}));
+app.use(cors());
+app.use(/^(?!\/api\/)/, express.static(staticFiles));
 
 app.ws('/', (ws, req) => {
   console.log('Подключилось');
