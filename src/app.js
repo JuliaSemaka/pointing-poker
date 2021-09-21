32ae17189dd41041a9a13759bac69878382247d0
Julia <ysemaka@mail.ru>
2021-09-21 16:36:31 +0300

    feat: correct

--- src/app.js
+++ src/app.js
@@ -24,7 +24,6 @@ app.use(express.json({ limit: '15mb' }));
 
 app.ws('/', (ws, req) => {
   console.log('Подключилось');
-  ws.send('hellohello');
   ws.on('message', (msg) => {
     msg = JSON.parse(msg);
     console.log(msg);
