856290cbdd140e8c12c411bbb43c985abd65cf4e
Julia <ysemaka@mail.ru>
2021-09-21 23:59:31 +0300

    feat: correct

--- src/app.js
+++ src/app.js
@@ -7,11 +7,11 @@ const PORT = process.env.PORT || 5000;
 const { addUser, connectionHandler, delUser } = require('./game/index');
 const { addMessage } = require('./chat/index');
 
-const staticFiles = path.resolve(__dirname, '../static');
+// const staticFiles = path.resolve(__dirname, '../static');
 
-app.use(json({ limit: '50mb' }));
+app.use(json({limit: '50mb'}));
 app.use(cors());
-app.use(/^(?!\/api\/)/, express.static(staticFiles));
+// app.use(/^(?!\/api\/)/, express.static(staticFiles));
 
 app.ws('/', (ws, req) => {
   console.log('Подключилось');
