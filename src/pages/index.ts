06175cdb9bc54bfa5e057d20584fc462cf722245
Iaroslav Silkin <maxfri1995@gmail.com>
2021-09-23 18:13:27 +0300

    init: init Main page container

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,3 +2,4 @@ export { Lobby } from './Lobby/Lobby';
 export { Game } from './Game/Game';
 export { Start } from './Start';
 export { ErrorPage } from './ErrorPage';
+export { MainPage } from './MainPage/MainPage';
