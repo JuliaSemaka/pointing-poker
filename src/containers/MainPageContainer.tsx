3aa9eb5296a7605a8abcd419be9567afed41bff9
Iaroslav Silkin <maxfri1995@gmail.com>
2021-09-24 18:48:58 +0300

    feat: add socket for main page lobby container

--- src/containers/MainPageContainer.tsx
+++ src/containers/MainPageContainer.tsx
@@ -1,80 +1,41 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router';
 import { reset } from 'redux-form';
 import ConnectToLobby from '../components/ConnectToLobby/ConnectToLobby';
-
-import { EHandleIssue } from '../components/UI/ui.module';
-import { MainPage } from '../pages';
-import { addGame, addMyId, addWebSocket } from '../store/actions/main';
 import { IReducer } from '../store/store.module';
+import { addMyId } from '../store/actions/main';
+import { MainPage } from '../pages';
 
 export const MainPageContainer: React.FC = () => {
+  const { socket, myId } = useSelector((state: IReducer) => state.main);
+  const history = useHistory();
   const [isModalOpened, setIsModalOpened] = useState(false);
   const [isObserver, setIsObserver] = useState(false);
   const [avatar, setAvatar] = useState('');
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    const socket = new WebSocket('ws://obscure-wave-90492.herokuapp.com/');
-    // const socket = new WebSocket('ws://localhost:5000/');
-    dispatch(addWebSocket(socket));
-    socket.onopen = () => {
-      console.log('Подключение установлено');
-      const id = (+new Date()).toString(16);
-      dispatch(addMyId(id));
-
-      const userData = {
-        firstName: 'July',
-        myId: id,
-        lastName: 'Yatsko',
-        jobTitle: 'web',
-        image: '',
-        player: 'diller',
-        title: 'Title 123456',
-        method: 'connection',
-      };
-      socket.send(JSON.stringify(userData));
-      socket.onmessage = (event) => {
-        const data = JSON.parse(event.data);
-        if (data.id) {
-          switch (data.method) {
-            case 'connection':
-              console.log(data);
-              dispatch(addGame(data));
-              break;
-            //     case 'add-user':
-            //       console.log('userId: ', userId);
-            //       console.log(data);
-            //       if (userId === null) {
-            //         setUserId(data.users[data.users.length - 1].id);
-            //       }
-            //       break;
-            //     case 'del-user':
-            //       console.log('userId: ', userId);
-            //       console.log(data);
-            //       setSessionId(null);
-            //       sock.onclose = function () {
-            //         console.log('WebSocket is closed now.');
-            //       };
-            //       break;
-            case 'add-message':
-              // dispatch(
-              //   addChatMessage({ idUser: data.idUser, message: data.message })
-              // );
-              break;
-          }
-        }
-      };
-    };
-  }, []);
-
   const onSubmit = () => {
+    const id = (+new Date()).toString(16);
+    dispatch(addMyId(id));
+
+    const userData = {
+      firstName: 'July',
+      myId: id,
+      lastName: 'Yatsko',
+      jobTitle: 'web',
+      image: '',
+      player: 'diller',
+      title: 'Title 123456',
+      method: 'connection',
+    };
+    socket.send(JSON.stringify(userData));
+    history.push("/lobby")
     console.log('Подключение к игре')
   }
 
   const handleStartGame = () => {
     handleCloseModal();
-    console.log('Старт игры')
   }
 
   const props = {
@@ -109,6 +70,7 @@ export const MainPageContainer: React.FC = () => {
 
   const handleSubmit = (event: any) => {
     onSubmit();
+    handleCloseModal();
     console.log('Форма отправлена')
   }
 
--- src/store/webSocket/index.ts
+++ src/store/webSocket/index.ts
@@ -13,20 +13,20 @@ export const useWebSocket = () => {
     dispatch(addWebSocket(socket));
     socket.onopen = () => {
       console.log('Подключение установлено');
-      const id = (+new Date()).toString(16);
-      dispatch(addMyId(id));
+      // const id = (+new Date()).toString(16);
+      // dispatch(addMyId(id));
 
-      const userData = {
-        firstName: 'July',
-        myId: id,
-        lastName: 'Yatsko',
-        jobTitle: 'web',
-        image: '',
-        player: 'diller',
-        title: 'Title 123456',
-        method: 'connection',
-      };
-      socket.send(JSON.stringify(userData));
+      // const userData = {
+      //   firstName: 'July',
+      //   myId: id,
+      //   lastName: 'Yatsko',
+      //   jobTitle: 'web',
+      //   image: '',
+      //   player: 'diller',
+      //   title: 'Title 123456',
+      //   method: 'connection',
+      // };
+      // socket.send(JSON.stringify(userData));
       socket.onmessage = (event) => {
         const data = JSON.parse(event.data);
         if (data.method) {
