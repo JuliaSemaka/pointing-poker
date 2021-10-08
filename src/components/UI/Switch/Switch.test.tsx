462d32ee4a44b53735ec79468df74eb68ac33e3f
maxfri <maxfri1995@gmail.com>
2021-10-09 02:01:03 +0300

    feat: add tests for ui components

--- src/components/UI/Switch/Switch.test.tsx
+++ src/components/UI/Switch/Switch.test.tsx
@@ -3,13 +3,13 @@ import { render } from '@testing-library/react';
 import Switch from './Switch';
 
 test('show switchers label', () => {
-  // const label = 'Test Message';
-  // const check = true;
-  // const handleClick = () => {
-  //   return;
-  // };
-  // const { getByText } = render(
-  //   <Switch isChecked={check} label={label} handleClick={handleClick} />,
-  // )
-  // expect(getByText(label)).toBeInTheDocument()
+  const label = 'Test Message';
+  const check = true;
+  const handleClick = () => {
+    return !check;
+  };
+  const { getByText } = render(
+    <Switch isChecked={check} label={label} handleClick={handleClick} input={handleClick} />,
+  )
+  expect(getByText(label)).toBeInTheDocument()
 });
